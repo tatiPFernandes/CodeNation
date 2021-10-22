fc86530fa56621f60e3d16e1c5b3aa9eda2226bf
Tati Fernandes <tatifernandes026@gmail.com>
2021-10-22 14:52:24 +0100

    up date activity 3

--- Activities_day4/activity3.js
+++ Activities_day4/activity3.js
@@ -1,36 +1,21 @@
-//const pin = 1233;
-//let balance = 1000;
-//let amount = 0;
-//newB = 0;
-//  
-//
-//const cashMachine = (amount) => {
-//        
-//if (pin == pin && amount <= balance ){
-//    newB = balance - amount;
-//    console.log(`Here you go!, your new balance is ${newB}`);
-//    }else if(pin !== 1233){
-//     console.log("Wrong Pin try again");
-//    }else{
-//        console.log("Your balance is not enough ")
-//    }
-//}
-//cashMachine(2000)
+const pin = 1233;
+let balance = 1000;
+let amount = 0;
+newB = 0;
+  
 
-let person = {
-    name: "Tati",
-    age: 34,
-    songs: [
-    "Tati",
-    "Africa",
-    "Drown"
-    ],
-    sayHi(){
-      return (`Hello my name is ${this.name}`);
-    }   
+const cashMachine = (amount) => { 
+        
+if (pin == pin && amount <= balance ){
+    newB = balance - amount;
+    console.log(`Here you go!, your new balance is ${newB}`);
+    }else if(pin !== 1233){
+     console.log("Wrong Pin try again");
+    }else{
+        console.log("Your balance is not enough ")
     }
+}
+cashMachine(200)
 
-console.log(person.sayHi())
 
-//console.log(person.songs[1])
  
