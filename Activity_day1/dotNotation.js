ee19fc2e853e1720c41e1f37ee286f398390783b
Tati Fernandes <tatifernandes026@gmail.com>
2021-10-22 16:31:20 +0100

    up  date

---
+++ Activity_day1/dotNotation.js
@@ -0,0 +1,8 @@
+let str = "Hello World";
+let letterD = str.slice(10,11);
+if(letterD.match("d")){
+    console.log(true)
+
+}else{
+    console.log(false)
+}
---
+++ Activity_day1/grid.js
@@ -0,0 +1,9 @@
+let arr = ["   |   |   ", "-----------"];
+
+for (let i = 1; i <= 11; i++) { 
+  if( i % 3 == 0){
+      console.log(arr[1]);
+  }else{
+      console.log(arr[0]);
+  }
+}
