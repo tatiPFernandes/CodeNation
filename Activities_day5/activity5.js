6a265d40fe3d5726796711f2e1eb83f4b3b529ee
Tati Fernandes <tatifernandes026@gmail.com>
2021-10-25 09:48:55 +0100

    up date

--- Activities_day5/activity5.js
+++ Activities_day5/activity5.js
@@ -1,24 +1,33 @@
-let coffeeShop ={
-
-    branch:{
-        drink:[
-           ["Latte: 2.50"],
-            "Tea: £1.50",
-            "Juice: £2.00"
-        ],
-
-        food: [
-            "Hash browns: £1.50",
-            "Quiche: £2.50",
-            "Croissant: £2.00"
-        ],
-        drinkOrdered(){
-            return(`Your drink order is ${this.drink[0]}`);
-        },
-        foodOrdered(){
-            return(`your food order is ${this.food[1]}`);
+let coffeeShop = {
+    branch: 'Costa',
+    drinks: ['Coffee', 2.50, 'Tea', 1.50, 'Hot Chocolate', 3],
+    food: ['Croissant', 3.50, 'Sandwich', 2],
+    drinksOrdered(drink){
+        let order = ''
+        let cost = 0
+        for(let i = 0; i < drink.length; i++){
+            for(let j = 0; j < this.drinks.length; j++){
+                if(drink[i] === this.drinks[j]){
+                    cost += this.drinks[j + 1]
+                    order = order + ` = ${drink[i]} £${this.drinks[j + 1]} = `
+                }
+            }
         }
+        return `Drink  ${order}\nCost  -  £${cost}\n`
+    },
+    foodOrdered(food){
+        let order = ''
+        let cost = 0
+        for(let i = 0; i < food.length; i++){
+            for(let j = 0; j < this.food.length; j++){
+                if(food[i] === this.food[j]){
+                    cost += this.food[j + 1]
+                    order = order + ` = ${food[i]} £${this.food[j + 1]} = `
+                }
+            }
+        }
+        return `Food  ${order}\nCost  -  £${cost}\n`
     }
 }
-console.log(coffeeShop.branch.drinkOrdered())
-console.log(coffeeShop.branch.foodOrdered())
+
+console.log(coffeeShop.foodOrdered(['Croissant', 'Sandwich']), coffeeShop.drinksOrdered(['Coffee', 'Tea']))
